d4144f0cce5cb2a858a5f0d92c439655b2810700
GAXMAN <siswasatu.smk1sumbawa@gmail.com>
2024-02-29 09:10:59 +0700

    tampilan awal
    tampilan awal web saya

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title> Nekopict </title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Caveat:wght@400..700&family=Lumanosimo&family=Playpen+Sans:wght@100..800&display=swap" rel="stylesheet">
+
+</head>
+<body>
+    <?php 
+    include "layout/header.html";
+    ?>
+    <main class="flex-shrink-0">
+  <div class="container">
+    <h1 class="mt-5">Selamat Datang Di Web Galery Nekopict</h1>
+    <p class="lead">Silahkan Login Jika Sudah Terdaftar.
+      <code class="small">Jika Belum Silahkan Register,
+      </code> Pada <code class="small">Halaman &gt; Yang Tersedia</code>.</p>
+  </div>
+</main>
+
+    <?php 
+    include "layout/footer.html";
+    ?>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+</body>
+</html>
---
+++ koneksi.php
@@ -0,0 +1,8 @@
+<?php
+    $conn=mysqli_connect("localhost","root","","ukk_gufran");
+// Check connection
+// if (!$conn) {
+//     die("Connection failed: " . mysqli_connect_error());
+//   }else {
+//     echo "berhasil";
+//   }
